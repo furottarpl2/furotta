59573d38b49281e67b7d17a86b93b0e46bfb849b
furottarpl2 <furotta05@gmail.com>
2022-05-13 10:02:09 +0700

    first commit

---
+++ app/Views/tesview.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document Title</title>
+</head>
+<body>
+  <table border="1">
+      <tr>
+          <th>No</th>
+          <th>Nama</th>
+          <th>Kelas</th>
+</tr>
+<tr>
+    <td>1</td>
+    <td>Elzha</td>
+    <td>XI RPL 2</td>
+</tr>
+</table>
+  
+</body>
+</html>
